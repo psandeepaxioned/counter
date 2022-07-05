24c0ef61dbb88e4f42eb93f833684eb6a051850e
psandeepaxioned <vikasb@axioned.com>
2022-07-05 11:58:59 +0530

    working on js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,29 @@
 /* Author: 
 
 */
-const counter = document.querySelectorAll('.twitter-number')
+const counter = document.querySelectorAll(".number");
+console.log(counter);
+const numbers = counter.innerText
+console.log(numbers);
 
+counter.forEach((counter) => {
+  // counter.innerText = "0";
 
+	const updateCounter = () => {
+		const target = +counter.innerText
+		console.log(target);
+		if(target)
+		setTimeout(updateCounter, 1);
 
+		// counter.innerText="0";
+		// const c = +counter.innerHTML;
 
+		// const increment = target/200;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		// if(c<target){
+		// 	counter.innerText = `${math.ceil(c + increment)}`;
+		// 	setTimeout(updateCounter,1)
+		// }
+	};
+	updatecounter();
+});
