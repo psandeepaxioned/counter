7bc2c67b8740b5f5989f1defc723693c6ef5f76f
psandeepaxioned <vikasb@axioned.com>
2022-07-05 12:22:05 +0530

    scripting for task done

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,27 +3,19 @@
 */
 const counter = document.querySelectorAll(".number");
 console.log(counter);
-const numbers = counter.innerText
-console.log(numbers);
 
 counter.forEach((counter) => {
-  // counter.innerText = "0";
+  counter.innerText = "0";
 
 	const updateCounter = () => {
-		const target = +counter.innerText
-		console.log(target);
-		if(target)
-		setTimeout(updateCounter, 1);
+		const target = +counter.getAttribute('data-target');
+		const c = +counter.innerText;
+		const increment = target/100;
 
-		// counter.innerText="0";
-		// const c = +counter.innerHTML;
-
-		// const increment = target/200;
-
-		// if(c<target){
-		// 	counter.innerText = `${math.ceil(c + increment)}`;
-		// 	setTimeout(updateCounter,1)
-		// }
+		if(c < target){
+			counter.innerText = `${Math.ceil(c + increment)}`;
+			setTimeout(updateCounter,1)
+		}
 	};
-	updatecounter();
+	updateCounter();
 });
